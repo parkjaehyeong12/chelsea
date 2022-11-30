2ebd0eb7c20d7df59af6a0797a2388a9e97a1ba7
parkjaehyeong12 <neronim2000@gmail.com>
2022-11-30 16:49:03 +0900

    11/30

--- c#/박재형 씨샵파일/박재형 씨샵/HelloMyCSharp11/HelloMyLastCSharp/Form1.cs
+++ c#/박재형 씨샵파일/박재형 씨샵/HelloMyCSharp11/HelloMyLastCSharp/Form1.cs
@@ -12,6 +12,10 @@ namespace HelloMyLastCSharp
 {
     public partial class Form1 : Form
     {
+        private bool checkIsBorrowed(Book b)
+        {
+            return b.isBorrowed;
+        }
         public Form1()
         {
             InitializeComponent();
@@ -23,17 +27,159 @@ namespace HelloMyLastCSharp
             label7.Text = "";
             label8.Text = "";
 
-            label5.Text = DataManager.Books.Count.ToString();
-            label6.Text = DataManager.Users.Count.ToString();
+            ScreenRefresh();
 
+            DataGridViewCellEventHandler dgclick1 = (sender, e) =>
+            {
+                Book book = dataGridView1.CurrentRow.DataBoundItem as Book;
+                textBox1.Text = book.Isbn;
+                textBox2.Text = book.Name;
+            };
 
+            dataGridView1.CellClick += dgclick1;
 
+            DataGridViewCellEventHandler dgclick2 = userDataGridViewCellClick;
+            dataGridView2.CellClick+=dgclick2;
+
+        }
+
+        private void userDataGridViewCellClick(object s, EventArgs e)
+        {
+            //이벤트를 발생시킨 주체
+            DataGridView thisGridView = s as DataGridView;
+            User u = thisGridView.CurrentRow.DataBoundItem as User;
+            textBox3.Text = u.Id.ToString();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             toolStripStatusLabel1.Text
                 = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+        }
+
+        private void 도서관리ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form2().ShowDialog(); //모달 창이 꺼져야 밑에 코드 실행
+            DataManager.Load();   //도서를 추가했으니 다시 불러온다
+
+            ScreenRefresh(); //도서 수, 회원 수등을 다시 리셋하는 함수
+
+        }
+
+        private void 사용자관리ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form3().ShowDialog(); //모달 창이 꺼져야 밑에 코드 실행
+            DataManager.Load();  //회원을 추가했으니 다시 불러온다
+            ScreenRefresh(); //도서 수, 회원 수등을 다시 리셋하는 함수
+
+        }
+        private void ScreenRefresh()
+        {
+            //전체 도서 수
+            label5.Text = DataManager.Books.Count.ToString() + " 권";
+
+            //전체 회원 수
+            label6.Text = DataManager.Users.Count.ToString() + " 명";
+
+            //대출중인 도서 수
+            //label7.Text =  DataManager.Books.Where(x=>x.isBorrowed).Count().ToString() +" 권";
+            label7.Text = DataManager.Books.Where(checkIsBorrowed).Count().ToString() + " 권";
+
+            //연체된 도서 수
+            label8.Text = DataManager.Books.Where(
+                delegate (Book x)
+                {
+                    return x.isBorrowed && x.BorrowedAt.AddDays(7) < DateTime.Now;
+                }).Count().ToString() + " 권";
+
+
+            dataGridView1.DataSource = null;
+            dataGridView2.DataSource = null;
+            if (DataManager.Books.Count > 0)
+                dataGridView1.DataSource = DataManager.Books;
+
+            if (DataManager.Users.Count > 0)
+                dataGridView2.DataSource = DataManager.Users;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "")
+                MessageBox.Show("isbn 없음");
+            else if (textBox3.Text.Trim() == "")
+                MessageBox.Show("사용자 id 입력해야 함");
+            else
+            {
+                try
+                {
+                    Book b = DataManager.Books.Single
+                        (x => x.Isbn == textBox1.Text);
+                    if (b.isBorrowed)
+                        MessageBox.Show("이미 다른 사람이 빌렸습니다.");
+                    else
+                    {
+                        User u = DataManager.Users.Single
+                            (x => x.Id.ToString() == textBox3.Text);
+                        b.UserId = u.Id;
+                        b.UserName = u.Name;
+                        b.isBorrowed = true;
+                        b.BorrowedAt = DateTime.Now;
+
+                        ScreenRefresh();
+                        DataManager.Save();
+                        MessageBox.Show($"{b.Name}이가 {u.Name}님께 대여됨");
+                    }
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("없는 책입니다.");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "")
+                MessageBox.Show("isbn 없음");
+            else
+            {
+                try
+                {
+                    Book b = DataManager.Books.Single
+                        (x => x.Isbn == textBox1.Text);
+                    if (b.isBorrowed) //빌렸을 경우에만 반납됨
+                    {
+                      // User u = DataManager.Users.Single
+                      //      (x => x.Id.ToString() == textBox3.Text);
+                        b.UserId = 0; //빌린 사람 없다는 뜻
+                        b.UserName = "";
+                        b.isBorrowed = false;
+                        DateTime oldDay = b.BorrowedAt; //빌린 날짜
+                        //현재 빌린 날짜 부분은 초기화함
+                        b.BorrowedAt = new DateTime();
+
+                        TimeSpan timeDiff = DateTime.Now - oldDay;
+
+                        ScreenRefresh();
+                        DataManager.Save();
+                        if (timeDiff.Days > 7)
+                            MessageBox.Show(b.Name + "연체 반납");
+                        else
+                            MessageBox.Show(b.Name + "정상 반납");
+                        //MessageBox.Show($"{b.Name}이가 {u.Name}님께 대여됨");
+                    }
+                    else
+                    {
+                        MessageBox.Show("대여 상태 아님");
+                    }
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("없는 책입니다.");
+                }
+            }
         }
     }
 }
